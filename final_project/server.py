fe501428fb5bc6de6a25ca68d3ca7218ca19f2f4
DBloeme <dennisbloeme@gmail.com>
2022-10-06 22:23:29 +0200

    Add files via upload

--- final_project/server.py
+++ final_project/server.py
@@ -7,18 +7,22 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
-
+    french_text = language_translator.translate(
+        text=(textToTranslate),
+        model_id='en-fr').get_result()
+    return french_text
+    
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
-
+    english_text = language_translator.translate(
+        text=(textToTranslate),
+        model_id='fr-en').get_result()
+    return english_text
+    
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    return render_template('index.html')# Write the code to render template
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
